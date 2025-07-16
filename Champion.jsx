f8688da51dfba9464f06c5e56a72d155c1939eb4
AndreiGulii <andreigulii1985@gmail.com>
2025-07-16 17:43:06 +0200

    first commit

---
+++ Champion.jsx
@@ -0,0 +1,92 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+const champions = [
+  {
+    id: 1,
+    name: "Вильгельм Стейниц",
+    yearsOfChampions: "1886—1894",
+    photoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b2/Wilhelm_Steinitz2.jpg",
+  },
+  {
+    id: 2,
+    name: "Эмануил Ласкер",
+    yearsOfChampions: "1894—1921",
+    photoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6e/Bundesarchiv_Bild_102-00457%2C_Emanuel_Lasker.jpg",
+  },
+  {
+    id: 3,
+    name: "Хосе Рауль Капабланка",
+    yearsOfChampions: "1921—1927",
+    photoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/2/24/Jos%C3%A9_Ra%C3%BAl_Capablanca_1931.jpg",
+  },
+  {
+    id: 4,
+    name: "Александр Алехин",
+    yearsOfChampions: "1927—1935, 1937—1946",
+    photoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/e/ee/Alexandre_Alekhine_Color.jpg",
+  },
+  {
+    id: 5,
+    name: "Макс Эйве",
+    yearsOfChampions: "1935—1937",
+    photoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/0/07/Jacob_Merkelbach%2C_Afb_B00000002112.jpg",
+  },
+];
+
+const Champion = ({ name, years, photo }) => {
+const [isEdit, setIsEdit]= React.useState(false);
+const [nameChanged, setName]= React.useState(name);
+const [yearsChanged, setYears]= React.useState(years);
+
+// 1. Создание абстрактной (не связанной с конкретным элементом) ссылки 
+const inputName= React.useRef();
+const inputYears= React.useRef();
+
+  return isEdit ?(
+    <div className="card mx-auto my-3" style={{ maxWidth: '400px' }}>
+      <img className="card-img-top" src={photo} alt={name} />
+      <div className="card-body">
+        <input ref={inputName} type="text" defaultValue={nameChanged}/>
+        <input ref={inputYears} type="text" defaultValue={yearsChanged} />
+        <button className="btn btn-success" onClick={()=>{
+          champions.name = setName(inputName.current.value);
+          years = setYears(inputYears.current.value);
+          setIsEdit(false);
+        }}>Save</button>
+      </div>
+    </div>
+  ) :(
+    <div className="card mx-auto my-3" style={{ maxWidth: '400px' }}>
+      <img className="card-img-top" src={photo} alt={name} />
+      <div className="card-body">
+        <h1 className="card-title">{nameChanged}</h1>
+        <p className="card-text">{yearsChanged}</p>
+        <button className="btn btn-warning" onClick={()=> setIsEdit(true)}>Edit Name or Years</button>
+      </div>
+    </div>
+
+  )
+};
+
+
+
+root.render(
+  <>
+    <h1 style={{textAlign: "center"}}>Чемпионы мира по шахматам</h1>
+    {champions.map(({ id, name, yearsOfChampions, photoUrl }) => (
+      <Champion
+        key={id}
+        name={name}
+        years={yearsOfChampions}
+        photo={photoUrl}
+      />
+    ))}
+  </>
+);
